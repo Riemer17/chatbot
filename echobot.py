53d197b2e23d2b760649b020f9f9e3b98a267a8b
Riemer K <riemer.kerkstra@gmail.com>
2019-05-14 10:07:06 +0200

    Echobot version 1.1
    - Added more responses in responses dictionary
    - Added random statements and questions
    - Made a dictionary with essential data e.g. name and creationdate

--- echobot.py
+++ echobot.py
@@ -1,28 +1,34 @@
 # Create your own Echobot
-import random, time, datetime, sqlite3
+import random, time, datetime, re
 
-
-creationdate = datetime.datetime(2019,5,6,8,15)
-
-name = "Echobot 1"
-responses = {"hey":["Hey","Hi","Hello"],"hi":["Hey","Hi","Hello"],"what is your name?":["My name is %s"%name,"People call me %s"%name],
+data = {"name" : "Echobot 1", "creationdate" : datetime.datetime(2019,5,6,8,15)}
+responses = {"hey" :["Hey","Hi","Hello"],"hi" :["Hey","Hi","Hello"],"hello" : ["Hey","Hi","Hello"],"what is your name?":["My name is %s"%data["name"],"People call me %s"%data["name"]],
              "how late is it?":["It is now %s"%datetime.datetime.now().strftime("%H:%M:%S"),"The time is %s"%datetime.datetime.now().strftime("%H:%M:%S")],
-             "how old are you?":["I am %s old"%(datetime.datetime.now()-creationdate),"Do you want to know that?"]}
+             "how old are you?":["I am %s old"%(datetime.datetime.now()-data["creationdate"]),"Do you want to know that?"],"how are you?" : ["I'm fine","I am fine","I am feeling well", "I am not feeling so well"]}
+
+statements = ["Tell me more!", "That sounds interesting", "Wow!", "How long have you felt this way?"]
+questions = ["How do you feel?", "What is your name"]
 def respond(message):
-    message = message.lower()
-    if message in responses:
-        return random.choice(responses[message])
-    else:
+    factor = random.randint(0,2)
+    print(factor)
+    if message.lower() in responses:
+        return random.choice(responses[message.lower()])
+    elif message.endswith("?"):
+        return ("I don't know the answer to %s :(" %message)
+    elif factor == 0:
+        return random.choice(statements)
+    elif factor == 1:
+        return random.choice(questions)
+    elif factor == 2:
         return("I can hear you! you said: %s"%message)
 
 def send_message(message):
     print("You: %s" %message)
-    waitingtime = random.uniform(0.5,1.5)
-    print(waitingtime)
-    time.sleep(waitingtime)
+    time.sleep(random.uniform(0.5,1.5))
     print("Echobot: %s" %respond(message))
 
-print("I am %s, your virtual buddy. You can speak to me and I will talk back to you"%name)
+print("I am %s, your virtual buddy. You can speak to me and I will talk back to you"%data["name"])
 while True:
     message = input("")
     send_message(message)
+
