3703f91c26ff06aa1528e2b60716e0d5fd5ead83
Riemer K <riemer.kerkstra@gmail.com>
2019-05-21 10:13:20 +0200

    Echobot version 1.2
    -  If a question is recognized the echobot will return I do not know the answer to {question}.
    - Added pattern matching and grammatical transformation. If a pattern is recognized the echobot will return a pre determined message +
      something from the message of the user. For example: User: I feel happy ; Echobot: Why do you feel happy?
    - If there is no pattern recognized the echobot will mirror the message with grammatical transformation
    - If the mirrored message is the same as the original message the echobot the echobot will return one of the pre determined
      statements or questions

--- echobot.py
+++ echobot.py
@@ -4,23 +4,43 @@ import random, time, datetime, re
 data = {"name" : "Echobot 1", "creationdate" : datetime.datetime(2019,5,6,8,15)}
 responses = {"hey" :["Hey","Hi","Hello"],"hi" :["Hey","Hi","Hello"],"hello" : ["Hey","Hi","Hello"],"what is your name?":["My name is %s"%data["name"],"People call me %s"%data["name"]],
              "how late is it?":["It is now %s"%datetime.datetime.now().strftime("%H:%M:%S"),"The time is %s"%datetime.datetime.now().strftime("%H:%M:%S")],
-             "how old are you?":["I am %s old"%(datetime.datetime.now()-data["creationdate"]),"Do you want to know that?"],"how are you?" : ["I'm fine","I am fine","I am feeling well", "I am not feeling so well"]}
+             "how old are you?":["I am %s old"%(datetime.datetime.now()-data["creationdate"]),"Do you want to know that?"],"how are you?" : ["I'm fine","I am fine","I am feeling well", "I am not feeling so well"],
+             "i am fine":["That's nice to hear","Why are you feeling fine?","Can you describe that?","Keep feeling fine!"]}
 
-statements = ["Tell me more!", "That sounds interesting", "Wow!", "How long have you felt this way?"]
-questions = ["How do you feel?", "What is your name"]
+statementsandquestions = ["Tell me more!", "That sounds interesting", "Wow!", "How long have you felt this way?","How do you feel?",
+                          "What is your name"]
+
+
+patterns = {"do you remember (.*)" : "How could I forget %s","my name is (.*)" : "your name is %s", "i feel (.*)":"Why do you feel %s?"}
+def swap_pronouns(phrase):
+    if "i" in phrase:
+        phrase = re.sub("i ","you ", phrase)
+    if " my" in phrase:
+        phrase = re.sub("my ","your ", phrase)
+    if " am" in phrase:
+        phrase = re.sub("am ","are ", phrase)
+    if " me " in phrase:
+        phrase = re.sub("me ","you ", phrase)
+
+    return phrase
+
 def respond(message):
-    factor = random.randint(0,2)
-    print(factor)
+
     if message.lower() in responses:
         return random.choice(responses[message.lower()])
-    elif message.endswith("?"):
+
+    for pattern in patterns:
+        phrase = re.search(pattern, message.lower())
+        if phrase:
+            return (patterns[pattern] % swap_pronouns(phrase.group(1)))
+
+    if message.endswith("?"):
         return ("I don't know the answer to %s :(" %message)
-    elif factor == 0:
-        return random.choice(statements)
-    elif factor == 1:
-        return random.choice(questions)
-    elif factor == 2:
-        return("I can hear you! you said: %s"%message)
+    else:
+        if swap_pronouns(message.lower()) == message.lower():
+            return random.choice(statementsandquestions)
+        else:
+            return swap_pronouns(message.lower()).capitalize()
 
 def send_message(message):
     print("You: %s" %message)
