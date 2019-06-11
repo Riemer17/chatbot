a9825f948796c5f41f8c968b9981c9b126dc5572
Riemer K <riemer.kerkstra@gmail.com>
2019-06-11 10:32:26 +0200

    Echobot version 1.3
    Added questionpatterns.
    When a message ends with a ? there is checked if the message matches a pattern. If it matches it will
    return on off the predetermined answers. The group(1) is put in the message at a predetetermined place
    with %s.

--- echobot.py
+++ echobot.py
@@ -1,42 +1,54 @@
-# Create your own Echobot
 import random, time, datetime, re
 
-data = {"name" : "Echobot 1", "creationdate" : datetime.datetime(2019,5,6,8,15)}
+data = {"name" : "Virtual buddy", "creationdate" : datetime.datetime(2019,5,6,8,15)}
 responses = {"hey" :["Hey","Hi","Hello"],"hi" :["Hey","Hi","Hello"],"hello" : ["Hey","Hi","Hello"],"what is your name?":["My name is %s"%data["name"],"People call me %s"%data["name"]],
              "how late is it?":["It is now %s"%datetime.datetime.now().strftime("%H:%M:%S"),"The time is %s"%datetime.datetime.now().strftime("%H:%M:%S")],
              "how old are you?":["I am %s old"%(datetime.datetime.now()-data["creationdate"]),"Do you want to know that?"],"how are you?" : ["I'm fine","I am fine","I am feeling well", "I am not feeling so well"],
              "i am fine":["That's nice to hear","Why are you feeling fine?","Can you describe that?","Keep feeling fine!"]}
 
-statementsandquestions = ["Tell me more!", "That sounds interesting", "Wow!", "How long have you felt this way?","How do you feel?",
+statementsandquestions = ["Tell me more!", "That sounds interesting", "Wow!","How do you feel?",
                           "What is your name"]
 
 
-patterns = {"do you remember (.*)" : "How could I forget %s","my name is (.*)" : "your name is %s", "i feel (.*)":"Why do you feel %s?"}
-def swap_pronouns(phrase):
-    if "i" in phrase:
-        phrase = re.sub("i ","you ", phrase)
-    if " my" in phrase:
-        phrase = re.sub("my ","your ", phrase)
-    if " am" in phrase:
-        phrase = re.sub("am ","are ", phrase)
-    if " me " in phrase:
-        phrase = re.sub("me ","you ", phrase)
+statementpatterns = {"my name is (.*)" : ["your name is %s"], "i feel (.*)":["Why do you feel %s?","How long have you felt %s?","You feel %s, why?"],
+            "i am feeling (.*)":["Why do you feel %s?","How long have you felt %s?","You feel %s, why?"]}
 
+questionpatterns = {"why do you feel (.*)\?":["I feel %s because it's raining", "I feel %s because it's sunny", "I feel %s because it's cloudy", "I feel %s because I am listening"],
+                    "why do you feel (happy)\?":["I feel %s because I am programming","I feel %s because I like what I am doing", "I feel %s because the weather is good",
+                                                   "I feel %s because I am helping someone"],
+                    "why do you feel (bad)\?":["I feel %s because what I am doing is boring","I feel %s because life is bad", "I feel %s because I do not like what I am doing",
+                                               "I feel %s because the water is bad",],
+                    "do you remember (.*)\?": ["How could I forget %s", "Of course I remember %s"]}
+
+def swap_pronouns(phrase):
+    if phrase == "me":
+        phrase = "you"
+    if "i " in phrase:
+        phrase = re.sub("i ","you ", phrase)
+    if "my" in phrase:
+        phrase = re.sub("my ","your ", phrase)
+    if "am" in phrase:
+        phrase = re.sub("am ","are ", phrase)
+    if "me " in phrase:
+        phrase = re.sub("me ","you ", phrase)
     return phrase
 
 def respond(message):
-
     if message.lower() in responses:
         return random.choice(responses[message.lower()])
 
-    for pattern in patterns:
-        phrase = re.search(pattern, message.lower())
-        if phrase:
-            return (patterns[pattern] % swap_pronouns(phrase.group(1)))
-
     if message.endswith("?"):
+        for pattern in questionpatterns:
+            phrase = re.search(pattern, message.lower())
+            if phrase:
+                return (random.choice(questionpatterns[pattern])% swap_pronouns(phrase.group(1).lower()))
         return ("I don't know the answer to %s :(" %message)
     else:
+        for pattern in statementpatterns:
+            phrase = re.search(pattern, message.lower())
+            if phrase:
+                return (random.choice(statementpatterns[pattern]) % swap_pronouns(phrase.group(1)))
+
         if swap_pronouns(message.lower()) == message.lower():
             return random.choice(statementsandquestions)
         else:
@@ -45,10 +57,11 @@ def respond(message):
 def send_message(message):
     print("You: %s" %message)
     time.sleep(random.uniform(0.5,1.5))
-    print("Echobot: %s" %respond(message))
+    respondmessage = respond(message)
+    print("Echobot: %s" %respondmessage)
 
 print("I am %s, your virtual buddy. You can speak to me and I will talk back to you"%data["name"])
+
 while True:
     message = input("")
     send_message(message)
-
