1e8c79688bdd983265afa10377940b6895df7994
Riemer K <riemer.kerkstra@gmail.com>
2019-06-11 11:26:19 +0200

    Echobot version 1.4
    The question mark at the end of the predetermined questions in the responses dictionary
    (how are you?, how old are you?) are removed. When a user sends them with question mark the
    if message.endswith("?") is triggered and when the message[:-1] (message without ?) is in the responses
    dictionary one of the answers is returned

--- echobot.py
+++ echobot.py
@@ -2,14 +2,14 @@ import random, time, datetime, re
 
 data = {"name" : "Virtual buddy", "creationdate" : datetime.datetime(2019,5,6,8,15)}
 responses = {"hey" :["Hey","Hi","Hello"],"hi" :["Hey","Hi","Hello"],"hello" : ["Hey","Hi","Hello"],"what is your name?":["My name is %s"%data["name"],"People call me %s"%data["name"]],
-             "how late is it?":["It is now %s"%datetime.datetime.now().strftime("%H:%M:%S"),"The time is %s"%datetime.datetime.now().strftime("%H:%M:%S")],
-             "how old are you?":["I am %s old"%(datetime.datetime.now()-data["creationdate"]),"Do you want to know that?"],"how are you?" : ["I'm fine","I am fine","I am feeling well", "I am not feeling so well"],
+             "how late is it":["It is now %s"%datetime.datetime.now().strftime("%H:%M:%S"),"The time is %s"%datetime.datetime.now().strftime("%H:%M:%S")],
+             "how old are you":["I am %s old"%(datetime.datetime.now()-data["creationdate"]),"Do you want to know that?"],"how are you" : ["I'm fine","I am fine","I am feeling well", "I am not feeling so well"],
              "i am fine":["That's nice to hear","Why are you feeling fine?","Can you describe that?","Keep feeling fine!"]}
 
 statementsandquestions = ["Tell me more!", "That sounds interesting", "Wow!","How do you feel?",
                           "What is your name"]
 
-
+# "i feel (.*) because (.*)":["You feel %s because %s"],
 statementpatterns = {"my name is (.*)" : ["your name is %s"], "i feel (.*)":["Why do you feel %s?","How long have you felt %s?","You feel %s, why?"],
             "i am feeling (.*)":["Why do you feel %s?","How long have you felt %s?","You feel %s, why?"]}
 
@@ -42,12 +42,18 @@ def respond(message):
             phrase = re.search(pattern, message.lower())
             if phrase:
                 return (random.choice(questionpatterns[pattern])% swap_pronouns(phrase.group(1).lower()))
+        if message[:-1].lower() in responses:
+            return random.choice(responses[message[:-1].lower()])
         return ("I don't know the answer to %s :(" %message)
     else:
         for pattern in statementpatterns:
             phrase = re.search(pattern, message.lower())
             if phrase:
-                return (random.choice(statementpatterns[pattern]) % swap_pronouns(phrase.group(1)))
+                # try:
+                #     if phrase.group(2):
+                #         return (random.choice(questionpatterns[pattern])% (swap_pronouns(phrase.group(1).lower()), swap_pronouns(phrase.group(2).lower())))
+                # except:
+                    return (random.choice(statementpatterns[pattern]) % swap_pronouns(phrase.group(1)))
 
         if swap_pronouns(message.lower()) == message.lower():
             return random.choice(statementsandquestions)
